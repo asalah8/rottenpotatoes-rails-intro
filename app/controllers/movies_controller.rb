819454b4e87df2c51e20e91a36f6606ec586f03f
asalah8 <asalah@uncc.edu>
2017-03-24 03:26:59 +0000

    part 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,45 +1,51 @@
+# This file is app/controllers/movies_controller.rb
 class MoviesController < ApplicationController
-
-  def movie_params
-    params.require(:movie).permit(:title, :rating, :description, :release_date)
-  end
-
+  
   def show
-    id = params[:id] # retrieve movie ID from URI route
-    @movie = Movie.find(id) # look up movie by unique ID
-    # will render app/views/movies/show.<extension> by default
+  id = params[:id] # retrieve movie ID from URI route
+  @movie = Movie.find(id) # look up movie by unique ID
+  # will render app/views/movies/show.html.haml by default
   end
-
+  
+  def new
+  @movie = Movie.new
+  # default: render 'new' template
+  end 
+  
   def index
     @movies = Movie.all
   end
-
-  def new
-    # default: render 'new' template
-  end
-
+  
   def create
-    @movie = Movie.create!(movie_params)
-    flash[:notice] = "#{@movie.title} was successfully created."
-    redirect_to movies_path
+ @movie = Movie.create!(user_params)
+ flash[:notice] = "#{@movie.title} was successfully created."
+ redirect_to movies_path
   end
-
+  
   def edit
-    @movie = Movie.find params[:id]
+ @movie = Movie.find params[:id]
   end
 
-  def update
-    @movie = Movie.find params[:id]
-    @movie.update_attributes!(movie_params)
-    flash[:notice] = "#{@movie.title} was successfully updated."
-    redirect_to movie_path(@movie)
-  end
+ def update
+ @movie = Movie.find params[:id]
+ @movie.update_attributes!(user_params)
 
-  def destroy
-    @movie = Movie.find(params[:id])
-    @movie.destroy
-    flash[:notice] = "Movie '#{@movie.title}' deleted."
-    redirect_to movies_path
-  end
+flash[:notice] = "#{@movie.title} was successfully updated."
+ redirect_to movie_path(@movie)
+ end
 
+def destroy
+ @movie = Movie.find(params[:id])
+ @movie.destroy
+ flash[:notice] = "Movie ’#{@movie.title}’ deleted."
+ redirect_to movies_path
+end
+
+
+end
+
+private
+def user_params
+ params.require(:movie)
+ params[:movie].permit(:title,:rating,:release_date)
 end
