639755b517d9020e7929ace1fd5f351ea11e110e
asalah8 <asalah@uncc.edu>
2017-03-27 00:20:40 +0000

    updated files

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
-Myrottenpotatoes::Application.routes.draw do
+Rottenpotatoes::Application.routes.draw do
   resources :movies
+  # map '/' to be a redirect to '/movies'
   root :to => redirect('/movies')
 end
